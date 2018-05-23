2db37a67dca4ae3043f2a887db51ecfc50863ab8
Meryem Mansouri Idrissi <midri.meryem@gmail.com>
2018-05-23 11:43:23 +0200

    Update Login.jsx

--- Login.jsx
+++ Login.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { ImageBackground } from 'react-native'
+import ReactFontFace from 'react-font-face'
 import {
   AppRegistry,
   StyleSheet,
@@ -13,25 +15,45 @@ class Register extends Component{
     //no need for state
   }
   render(){
+    const resizeMode = 'cover';
     return (
     //Container layout
+      <ImageBackground
+            source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg' }}
+            imageStyle={{
+              resizeMode:'stretch',
+            }}
+        >
+
      <View style = {{
             flex: 1,
             flexDirection: 'column',
             justifyContent: 'center',
             alignItems: 'center'
-          }}>
-        <form>
-          <label> Username: 
-            <input type="text" name="name" />
-          </label>
-          <label> Password: 
-            <input type="password" name="Password" />
-          </label>
-          <input type="submit" value="Submit"/>
-        </form>
+      }}>
+           <form>
+              <label> Username: 
+                <input type="text" name="name" />
+              </label>
+              <label> Password: 
+                <input type="password" name="Password" />
+              </label>
+              <input type="submit" value="Submit"/>
+          </form>
       </View>
+    </ImageBackground>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  title: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  submit: {
+    color: 'red',
+  },
+});
 AppRegistry.registerComponent('App', () => Register)
