751f9809ffa260c8b8af276f05908c2230b0a389
Meryem Mansouri Idrissi <midri.meryem@gmail.com>
2018-05-25 14:48:38 +0200

    Create picker.js
    Learning to work with a picker

---
+++ picker.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import {Picker} from 'react-native'
+import { ImageBackground } from 'react-native'
+import ReactFontFace from 'react-font-face'
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native'
+
+class Register extends Component{
+  constructor(props){
+    super(props);
+    //no need for state
+    this.state={
+     Payment_types:  [{CardType: 'SEPA', CardName: 'SEPA'}, {CardType: 'Paypal', CardName: 'PAYPAL'}, {CardType: 'CreditCard', CardName: 'Credit Card'}],
+    }
+  }
+  loadPaymentMethods(){
+    return this.state.Payment_types.map(
+      PaymentT => (
+       <Picker.Item label={PaymentT.CardName} value={PaymentT.CardType} />
+      )
+    )
+  }
+  render(){
+    const resizeMode = 'cover';
+    return (
+      <View>
+    <Picker
+      selectedValue={this.state.selectedUserType}
+      onValueChange={(itemValue, itemIndex) => 
+          this.setState({selectedUserType: itemValue})}>
+
+      // Dynamically loads Picker.Values from this.state.userTypes.
+
+      {this.loadPaymentMethods()}
+    </Picker>
+  </View>
+      )
+  }
+}
+
+const styles = StyleSheet.create({
+  title: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  submit: {
+    color: 'red',
+  },
+});
+AppRegistry.registerComponent('App', () => Register)
